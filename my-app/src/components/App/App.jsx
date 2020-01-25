4ba3156a2c1e845bfd65672785513dca8c24feef
Йошкарали <dashkevich672@gmail.com>
2020-01-25 16:47:09 +0300

    hw-05

--- my-app/src/components/App/App.jsx
+++ my-app/src/components/App/App.jsx
@@ -33,6 +33,7 @@ class App extends Component {
       contact =>
         contact.name.toLocaleLowerCase() !== item.name.toLocaleLowerCase(),
     );
+
     if (isNotExist) {
       const itemToAdd = { ...item, id: uuid() };
       this.setState(state => ({
@@ -52,6 +53,9 @@ class App extends Component {
   render() {
     const { contacts, filter, contactAlreadyExist } = this.state;
     const filtratedContacts = this.filterContacts(contacts, filter);
+    setTimeout(() => {
+      this.setState({ contactAlreadyExist: false });
+    }, 7000);
     return (
       <div className={styles.container}>
         <Logo />
